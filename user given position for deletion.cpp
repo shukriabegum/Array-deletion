42d2279e6093eb28312f0073682030886a7f4b4b
Shukria Begum <shukriabegum2004@gmail.com>
2025-09-27 21:13:19 +0600

    Add files via upload
    add file

---
+++ user given position for deletion.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<"Enter a size:";
+    cin>>n;
+    int a[n];
+    cout<<"Element of array;";
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int pos;
+    cout<<"Enter position:";
+    cin>>pos;
+    while(pos<1||pos>n)
+    {
+        cout<<"Invalid position"<<" ";
+        cout<<"Enter position:";
+        cin>>pos;
+
+    }
+    for(i=pos-1;i<n;i++)
+    {
+        a[i]=a[i+1];
+    }
+    cout<<"After deletion:";
+    for(i=0;i<n-1;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
+
